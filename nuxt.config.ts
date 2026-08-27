cfb3742b1b70ef264508dd314459c1382daaebb0
meihao550 <ziyin550@gmail.com>
2026-08-28 03:31:02 +0900

    feat: Nuxt+TailwindCSSの導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss']
 })
