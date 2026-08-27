878bc0b9bae00058ab354f1b9ed04a953863922d
meihao550 <ziyin550@gmail.com>
2026-08-28 03:30:11 +0900

    feat: ヘッダーの骨組みを作成

--- app/app.vue
+++ app/app.vue
@@ -4,6 +4,17 @@
 
 <template>
   <div>
+    <header>
+      <div class="p-4">
+        <a href="/">
+          <span class="text-[15px]">Meihao-Portfolio</span>
+        </a>
+        <nav>
+          <a>執筆・記事</a>
+        </nav>
+      </div>
+
+    </header>
     <NuxtPage />
   </div>
 </template>
